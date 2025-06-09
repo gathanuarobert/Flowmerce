d5f3df7f6ad88fc2f2a981bdc8ac22672d993e80
gathanuarobert <robertgathanua@gmail.com>
2025-06-09 13:29:02 +0300

    Register Order model in Admin with custom OrderAdmin display.

--- backend/flowmerce/core/admin.py
+++ backend/flowmerce/core/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import User, Product, Category, Tag
+from .models import User, Product, Category, Tag, Order
 
 admin.site.register(User)
 
-@admin.site.register(Product)
+
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('image', 'title', 'category', 'status', 'stock', 'price')
     list_filter = ('category', 'status')
@@ -12,6 +13,13 @@ class ProductAdmin(admin.ModelAdmin):
 
 admin.site.register(Category)
 admin.site.register(Tag)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'product_title', 'quantity', 'status', 'amount', 'order_date')
+    list_filter = ('order_date', 'status', 'product__title')
+    search_fields = ('product__title', 'order_date', 'employee')
 
 admin.site.site_header = 'Flowmerce Admin'
 admin.site.site_title = 'Flowmerce Admin Portal'
