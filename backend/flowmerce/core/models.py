b79a60ccd57805ee95c9ca587fb1a0861fcb7c5b
gathanuarobert <robertgathanua@gmail.com>
2025-06-09 13:26:51 +0300

    Create an Orders model with incorporation of slug and class Meta.

--- backend/flowmerce/core/models.py
+++ backend/flowmerce/core/models.py
@@ -96,3 +96,31 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Order(models.Model):
+    employee = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    product_title = models.CharField(max_length=255)
+    product = models.ForeignKey(Product, related_name='orders', on_delete=models.CASCADE)
+    product_price = models.PositiveIntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    amount = models.PositiveIntegerField(default=0)
+    status = models.CharField(max_length=255,choices=[('pending', 'Pending'), ('completed', 'Completed'), ('cancelled', 'Cancelled')])
+    order_date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name ='Order'
+        verbose_name_plural = 'Orders'
+        
+    def __str__(self):
+        return f'Order {self.id} by {self.user.email}'
+    
+    def save (self, *args, **kwargs):
+        if self.product_title and self.quantity:
+           self.amount = self.quantity * self.product.price 
+        super().save(*args, **kwargs)   
+               
+    
