5924bec3d4992f2640a9eb93ee777581561f5e0b
gathanuarobert <robertgathanua@gmail.com>
2025-06-10 23:01:18 +0300

    Make changes to OrderSerializer and OrderItemSerializer and nest OrderItemSerializer into OrderSerializer.

--- backend/flowmerce/core/serializers.py
+++ backend/flowmerce/core/serializers.py
@@ -11,24 +11,35 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'    
 
-class OrderSerializer(serializers.ModelSerializer):
-    product_title = serializers.CharField(source='product.title', read_only=True)
-    employee = serializers.CharField(source='employee.email', read_only=True)
-    product_price = serializers.IntegerField(source='product.price', read_only=True)
-    quantity = serializers.IntegerField(default=1)
-    amount = serializers.IntegerField(read_only=True)
-    order_date = serializers.DateTimeField(read_only=True)
-    status = serializers.ChoiceField(choices=Order.STATUS_CHOICES, default='pending')
-    created_at = serializers.DateTimeField(read_only=True)
-    
-    class Meta:
-        model = Order
-        fields = '__all__'
-
 class OrderItemSerializer(serializers.ModelSerializer):
     product_title = serializers.CharField(source='product.title', read_only=True)
-    product_price = serializers.IntegerField(source='product.price', read_only=True)
+    product_price = serializers.DecimalField(source='product.price', read_only=True, max_digits=10, decimal_places=2)
+    product_image = serializers.ImageField(source='product.image', read_only=True)
+    order_id = serializers.IntegerField(source='order.id', read_only=True)
+    quantity = serializers.IntegerField(default=1)
+    price = serializers.DecimalField(read_only=True, max_digits=10, decimal_places=2)
+
+    def validate_quantity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Quantity must be greater than zero.")
+        return value
 
     class Meta:
         model = OrderItem
-        fields = '__all__'   
+        fields = ['id', 'order_id', 'product', 'product_title', 'product_price', 'product_image', 'quantity', 'price']
+        extra_kwargs = {
+            'product': {'write_only': True}
+        }  
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    employee_email = serializers.CharField(source='employee.email', read_only=True)
+    amount = serializers.DecimalField(read_only=True, max_digits=10, decimal_places=2)
+    status = serializers.ChoiceField(choices=Order.STATUS_CHOICES, default='pending')
+
+    class Meta:
+        model = Order
+        fields = ['id','employee', 'employee_email', 'amount', 'status', 'items']
+        extra_kwargs = {
+            'employee': {'write_only': True}
+        }
