f3f19318c578db7726e96d2e3a9c953ad6412585
gathanuarobert <robertgathanua@gmail.com>
2025-06-10 18:28:58 +0300

    Add UserListView with pagination and JWT token creation on user registration.

---
+++ backend/flowmerce/core/views.py
@@ -0,0 +1,33 @@
+from rest_framework import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import generics
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User, Product, Order, OrderItem
+from .serializers import UserSerializer, ProductSerializer, Orderserializer, OrderItemSerializer
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class UserListView(generics.ListAPIView):
+    permission_classes = permissions.AllowAny
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    pagination_class = CustomPagination
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token = RefreshToken.for_user(user)
+            return Response({
+                'user': serializer.data,
+                'access': str(token.access_token),
+                'refresh': str(token)
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
