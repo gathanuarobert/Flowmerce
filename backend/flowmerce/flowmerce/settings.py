f3b9bf2e4165dd1afbaaf28f74c7e7244055a90d
gathanuarobert <robertgathanua@gmail.com>
2025-06-10 19:10:06 +0300

    Incorporate REST_FRAMEWORK to ensure proper pagination.

--- backend/flowmerce/flowmerce/settings.py
+++ backend/flowmerce/flowmerce/settings.py
@@ -52,6 +52,14 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'core.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
