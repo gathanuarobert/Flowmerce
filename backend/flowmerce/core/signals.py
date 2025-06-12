6b2d2094daf3b7dd76f05a0fd1804ef9f5f5c48f
gathanuarobert <robertgathanua@gmail.com>
2025-06-12 13:48:38 +0300

    Implement order_confirmation_email(order) to sent emails to employee once an order is made.

--- backend/flowmerce/core/signals.py
+++ backend/flowmerce/core/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save, pre_save
 from django.utils.text import slugify
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import User, Order, Product
 
 @receiver(post_save, sender=User)
@@ -28,3 +30,24 @@ def product_stock_update(sender, instance, created, **kwargs):
 def set_product_slug(sender, instance, **kwargs):
     if not instance.slug:
         instance.slug = slugify(instance.title)
+
+@receiver(post_save, sender=Order)
+def new_order(sender, instance, created, **kwargs):
+    if created:
+        print(f'New order created: {instance.number} for product {instance.product.title} by {instance.employee.email}')  
+
+def order_confirmation_email(order):
+    subject = f'Order Confirmation: {order.number}'
+    message = (
+        f'Your order for {order.product.title} has been placed successfully.\n'
+        f'Order Number: {order.number}\n'
+        f'Product: {order.product.title}'
+        )           
+    recipient = order.employee.email
+    send_mail(
+        subject,
+        message,
+        settings.DEFAULT_FROM_EMAIL,
+        [recipient],
+        fail_silently=False,
+    )
