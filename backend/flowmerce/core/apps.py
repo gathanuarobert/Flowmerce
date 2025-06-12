5b34b6dfbc9fbc0ffe9e782a6b7ddf57a8674d2f
gathanuarobert <robertgathanua@gmail.com>
2025-06-12 08:16:03 +0300

    Add CoreConfig app configuration for core app.

---
+++ backend/flowmerce/core/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'flowmerce.core'
+    verbose_name = 'Flowmerce Core'
+    
