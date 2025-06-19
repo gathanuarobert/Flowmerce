0328d2f3842018f9f9b3ad4cfd5dbef8e12feded
gathanuarobert <robertgathanua@gmail.com>
2025-06-19 17:24:19 +0300

    Integrate API handling for user registration.

--- frontend/Flowmerce/src/components/Register.jsx
+++ frontend/Flowmerce/src/components/Register.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import { FaUser, FaEnvelope, FaLock, FaArrowRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../utils/api';
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        email: "",
+    });
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await api.post('/register/', formData)
+            console.log('Registration successfully:', response.data);
+            navigate('/login')
+        } catch (error) {
+            console.error('Registration failed:', error.response.data)
+        }
+    }
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
+    }
+
   return (
     <div className="h-full flex items-center justify-center mt-5">
-      <form className="w-full max-w-md bg-white rounded-xl shadow p-8 space-y-6">
+      <form onSubmit={handleSubmit} className="w-full max-w-md bg-white rounded-xl shadow p-8 space-y-6">
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-2">Create Account</h1>
         
         {/* Name Field */}
@@ -14,8 +43,10 @@ const Register = () => {
             <FaUser className="text-gray-400" />
           </div>
           <input
+            name='name'
             type="text"
             placeholder="Full Name"
+            onChange={handleChange}
             className="w-full pl-10 pr-4 py-3 rounded-4xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-amber-500"
           />
         </div>
@@ -26,8 +57,10 @@ const Register = () => {
             <FaEnvelope className="text-gray-400" />
           </div>
           <input
+            name='email'
             type="email"
             placeholder="Email"
+            onChange={handleChange}
             className="w-full pl-10 pr-4 py-3 rounded-4xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-amber-500"
           />
         </div>
@@ -38,8 +71,10 @@ const Register = () => {
             <FaLock className="text-gray-400" />
           </div>
           <input
+            name="password"
             type="password"
             placeholder="Password"
+            onChange={handleChange}
             className="w-full pl-10 pr-4 py-3 rounded-4xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-amber-500"
           />
         </div>
