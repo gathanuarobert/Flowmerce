32948d370e746297285bd209f1e9baaf603dcdb7
gathanuarobert <robertgathanua@gmail.com>
2025-10-22 10:44:08 +0300

    EDit timeout limit in the api.

--- frontend/Flowmerce/src/utils/api.js
+++ frontend/Flowmerce/src/utils/api.js
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/',
-  timeout: 10000,
+  timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
