f3f5d0f7e9b483ee6628dc71f9b00dc37c71e7be
gathanuarobert <robertgathanua@gmail.com>
2026-01-21 12:32:23 +0300

    Edit settings.py file to handle middleware.

--- backend/flowmerce/flowmerce/settings.py
+++ backend/flowmerce/flowmerce/settings.py
@@ -89,6 +89,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'core.middleware.subscription_middleware.SubscriptionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
